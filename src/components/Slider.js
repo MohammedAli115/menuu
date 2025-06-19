4ae44d930202fd8c4c85b29006b2082a6b9a25c2
Mohammed Ali <mohamedalihasan15@gmail.com>
2025-06-19 14:43:19 +0300

    update slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,8 +4,8 @@ function Slider() {
   return (
     <div className="container-fluid p-0 mt-5 py-1 bg-dark">
       <img src="/images/imgTop.jpg" alt="banner"
-        className="position-relative w-100 text-center mx-auto d-block"
-        style={{ height: "250px"}}
+        className="position-relative  w-100 text-center mx-auto d-block"
+        style={{ height: "350px"}}
         />
     </div>
   );
